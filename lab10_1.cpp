75c89312e5d533c701f56385d2db51c7e62d7f39
PHANAGUS <phanarin03072002@gmail.com>
2021-12-14 16:17:32 +0700

    lab10_1.cpp completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main(){	
+	double loan,rate,pay,balance,inrt;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
+	balance = loan;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,25 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	for(int i=1;;i++)
+	{
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i;
+		cout << setw(13) << left << balance;
+		inrt = balance*(rate/100);
+		cout << setw(13) << left << inrt;
+		balance += inrt;
+		cout << setw(13) << left << balance;
+		if(balance <= pay)
+		{
+			pay = balance;
+		}
+		cout << setw(13) << left << pay;
+		balance -= pay;
+		cout << setw(13) << left << balance;
+		cout << "\n";
+		if(balance == 0) break;
+	}
 	
 	return 0;
 }
